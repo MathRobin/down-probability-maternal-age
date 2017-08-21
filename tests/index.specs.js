b62d38cbef4e5cb1be821ec8e9f4344c4b2fa3cf
mathrobin <mthrobin@gmail.com>
2017-08-21 14:10:18 +0200

    change tests values for more diversity

--- tests/index.specs.js
+++ tests/index.specs.js
@@ -60,15 +60,15 @@ describe('calculateDownProbability', function () {
 
     describe('calculateFromMonths', function () {
         it('should calculate from years and months', function () {
-            expect(testedThing.calculateFromMonths(243)).toEqual({
-                percent: 0.06626905235255136,
-                decimal: 0.0006626905235255136,
-                perBirth: '1/1509'
+            expect(testedThing.calculateFromMonths(185)).toEqual({
+                percent: 0.06476683937823835,
+                decimal: 0.0006476683937823834,
+                perBirth: '1/1544'
             });
-            expect(testedThing.calculateFromMonths(526)).toEqual({
-                percent: 3.3333333333333335,
-                decimal: 0.03333333333333333,
-                perBirth: '1/30'
+            expect(testedThing.calculateFromMonths(784)).toEqual({
+                percent: 7.6923076923076925,
+                decimal: 0.07692307692307693,
+                perBirth: '1/13'
             });
         });
 
